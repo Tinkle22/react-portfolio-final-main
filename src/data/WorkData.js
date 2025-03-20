94d37cfa3cf7f282e96f8e33c980b94e80332d67
Tinkle22 <104595797+Tinkle22@users.noreply.github.com>
2025-03-20 11:42:46 +0200

    Mutale_Profile

---
+++ src/data/WorkData.js
@@ -0,0 +1,25 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Brissp Academy Website",
+        description:"This Web App was Built for Brissp Academy. It is a fully responsive website with a backend built with javascript and SQL",
+        tags:["Next js","SQL","typescript"],
+        
+        demo:"https://briisp-academy.vercel.app/",
+        github:"https://briisp-academy.vercel.app/"
+    },
+    {
+        id:2,
+        name:"First City Lodge",
+        description:"A lodge booking website build using Next js with typescript. It has a really cool UI and smooth animations.",
+        tags:["react","typescript"],
+        
+        demo:"https://lodge-rho.vercel.app/",
+        github:"https://github.com/Tinkle22/first-city-lodge"
+        
+    },
+
+  
+] 
